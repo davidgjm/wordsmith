fa5c38f18ba8699cef8ad157f63a0f35e82eab76
David <dvdgjm@hotmail.com>
2023-11-04 17:01:18 +0800

    database switched to Postgresql

--- backend/src/main/java/com/tng/web/wordsmith/infrastructure/data/BaseEntity.java
+++ backend/src/main/java/com/tng/web/wordsmith/infrastructure/data/BaseEntity.java
@@ -1,21 +1,20 @@
 package com.tng.web.wordsmith.infrastructure.data;
 
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.Setter;
-import org.springframework.data.annotation.CreatedDate;
 
 import java.io.Serializable;
-import java.time.Instant;
 
 @Data
 @MappedSuperclass
 public abstract class BaseEntity implements Serializable {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(value = AccessLevel.PROTECTED)
     protected Long id;
 
